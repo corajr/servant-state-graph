44abfcb59d70f7c8600e57cc0ec1220084901457
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2017-02-11 23:22:09 -0500

    cleaning up graph module

--- src/Servant/StateGraph/Graph.hs
+++ src/Servant/StateGraph/Graph.hs
@@ -1,55 +1,56 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeOperators         #-}
 module Servant.StateGraph.Graph
   ( module Servant.StateGraph.Graph
-  , module Servant.StateGraph.Graph.Links
-  , module Servant.StateGraph.Graph.RichEndpoint
-  , module Servant.StateGraph.Graph.Types
+  , module X
   ) where
 
-import qualified Data.ByteString.Char8 as B
-import Control.Lens.TH
 
-import GHC.Generics
-import Data.Typeable
-import Data.Maybe
-import qualified Data.Text as T
-import Data.Proxy (Proxy(..))
-import Data.Graph.Inductive.Graph (empty, insNode, insEdge, mkGraph, Node, LEdge(..), LNode(..))
-import Data.Graph.Inductive.PatriciaTree
-import Control.Lens (view, use, uses, (.=), (<>=), (<<+=), (%=), (&), (<>~), (^.), (%~))
-import qualified Control.Lens as L
-import qualified Data.Map.Strict as Map
+import           Control.Monad.State
 
-import Control.Monad.State
+import           Control.Lens                          (use, uses, view, (%=),
+                                                        (%~), (&), (.=), (<<+=),
+                                                        (<>=), (<>~), (^.))
 
-import GHC.TypeLits
+import qualified Control.Lens                          as L
+import           Control.Lens.TH
 
-import qualified Network.HTTP.Types as HTTP
+import qualified Data.ByteString.Char8                 as B
+import           Data.Graph.Inductive.Graph            (LEdge (..), LNode (..),
+                                                        Node, empty, insEdge,
+                                                        insNode, mkGraph)
+import           Data.Graph.Inductive.PatriciaTree
+import qualified Data.Map.Strict                       as Map
+import           Data.Maybe                            (mapMaybe)
+import qualified Data.Text                             as T
 
-import Servant.API
-import Servant.Utils.Links
-import Servant.Docs.Internal
-import Servant.API.ContentTypes
-import Servant.StateGraph.Graph.RichEndpoint
-import Servant.StateGraph.Graph.Links
-import Servant.StateGraph.Graph.Types
+import           Data.Proxy                            (Proxy (..))
+import           Data.Typeable                         (TypeRep, Typeable,
+                                                        typeRep)
+import           GHC.Generics
+import           GHC.TypeLits
+
+
+import qualified Network.HTTP.Types                    as HTTP
+import           Servant.API
+
+import           Servant.StateGraph.Graph.Links        as X
+import           Servant.StateGraph.Graph.RichEndpoint as X
+import           Servant.StateGraph.Graph.Types        as X
 
 -- | State data for building up the graph.
 data GraphStateData = GraphStateData
-  { _endpoint :: Endpoint
-  , _action :: Action
-  , _typesToNodeIds :: Map.Map TypeRep Node
-  , _nodeTypes :: Map.Map TypeRep NodeType
-  , _edgesQueue :: [RichLink]
-  , _currentNodeId :: Int
-  , _currentGraph :: ApiGraph
+  { _typesToNodeIds :: Map.Map TypeRep Node
+  , _nodeTypes      :: Map.Map TypeRep NodeType
+  , _edgesQueue     :: [RichLink]
+  , _currentNodeId  :: Int
+  , _currentGraph   :: ApiGraph
   }
 $(makeLenses ''GraphStateData)
 
@@ -57,19 +58,17 @@ type GraphState = State GraphStateData
 
 -- | Color for each common type of request: green for GET, purple for POST, blue for PUT, red for DELETE.
 verbColors :: StdMethod -> String
-verbColors GET = "green"
-verbColors POST = "purple"
-verbColors PUT = "blue"
+verbColors GET    = "green"
+verbColors POST   = "purple"
+verbColors PUT    = "blue"
 verbColors DELETE = "red"
-verbColors _ = "black"
+verbColors _      = "black"
 
 -- | Generate a graph from a Servant API type.
 graph :: (HasGraph api api, LinksFor api) => Proxy api -> ApiGraph
 graph p = view currentGraph $ execState (graphFor p p >> connectLinks) startingState
   where
-    startingState = GraphStateData { _endpoint = defEndpoint
-                                   , _action = defAction
-                                   , _typesToNodeIds = withRoot
+    startingState = GraphStateData { _typesToNodeIds = withRoot
                                    , _nodeTypes = nodeTypesFromLinks apiLinks
                                    , _edgesQueue = apiLinks
                                    , _currentNodeId = 1
@@ -83,7 +82,7 @@ mkEdge :: Map.Map TypeRep Node -> RichLink -> Maybe (LEdge ApiEdge)
 mkEdge typesToNodeIds' l = do
   startNode <- Map.lookup sourceType typesToNodeIds'
   endNode <- Map.lookup targetType typesToNodeIds'
-  return $ (startNode, endNode, ApiEdge rel (verbColors meth))
+  return (startNode, endNode, ApiEdge rel (verbColors meth))
     where sourceType = l^.linkSource.returnType
           targetType = l^.linkTarget.returnType
           rel = l^.linkRel
@@ -96,59 +95,42 @@ connectLinks = do
   let edges = mapMaybe (mkEdge typesToNodeIds') xs
   mapM_ (\x -> currentGraph %= insEdge x) edges
 
--- https://github.com/haskell-servant/servant/blob/master/servant-docs/src/Servant/Docs/Internal.hs#L813
--- https://hackage.haskell.org/package/servant-docs-0.10/docs/src/Servant-Docs-Internal.html#docs
--- basically a reimplementation of HasDocs type class
-
+-- | Generate a graph for an API type.
 class HasGraph root api where
   graphFor :: Proxy root -> Proxy api -> GraphState ()
 
 -- Terminal instance for verbs (ending an endpoint path).
 instance (Typeable a, ReflectMethod method) => HasGraph root (Verb method status ctypes a) where
   graphFor rootP _ = do
-    nodeType <- uses nodeTypes (Map.findWithDefault NormalNode stateName)
-    existingNode <- uses typesToNodeIds (Map.lookup stateName)
+    let retType = typeRep (Proxy :: Proxy a)
+    nodeType <- uses nodeTypes (Map.findWithDefault NormalNode retType)
+    existingNode <- uses typesToNodeIds (Map.lookup retType)
     case existingNode of
-      Just _ -> return () -- no-op
+      Just _ -> return ()
       Nothing -> do
         currentNodeId' <- currentNodeId <<+= 1
-        let node = (currentNodeId', ApiNode (show stateName) nodeType)
+        let node = (currentNodeId', ApiNode (show retType) nodeType)
         currentGraph %= insNode node
-        typesToNodeIds %= Map.insert stateName currentNodeId'
-    endpoint .= defEndpoint
-    action .= defAction
-    where
-      stateName = typeRep p
-      p = Proxy :: Proxy a
+        typesToNodeIds %= Map.insert retType currentNodeId'
 
  -- Extract one component of the path.
 instance (KnownSymbol path, HasGraph root api) => HasGraph root (path :> api) where
-  graphFor rootP _ = do
-    endpoint.path <>= [symbolVal pa]
-    graphFor rootP subApiP
+  graphFor rootP _ = graphFor rootP subApiP
     where subApiP = Proxy :: Proxy api
           pa = Proxy :: Proxy path
 
 instance (KnownSymbol sym, HasGraph root api)
       => HasGraph root (QueryParam sym a :> api) where
-  graphFor rootP p = do
-    graphFor rootP subApiP
+  graphFor rootP p = graphFor rootP subApiP
     where subApiP = Proxy :: Proxy api
           paramP = Proxy :: Proxy (QueryParam sym a)
 
 instance (KnownSymbol sym, HasGraph root api) => HasGraph root (Capture sym a :> api) where
-  graphFor rootP _ = do
-    endpoint.path <>= [":" ++ symbolVal sym']
-    graphFor rootP subApiP
+  graphFor rootP _ = graphFor rootP subApiP
     where subApiP = Proxy :: Proxy api
           sym' = Proxy :: Proxy sym
 
 -- Compute subgraphs for each piece of the "alternatives" (:<|>) in an API type, then join them.
 instance (HasGraph root a, HasGraph root b) => HasGraph root (a :<|> b) where
-  graphFor rootP _ = do
-    graphFor rootP p1
-    graphFor rootP p2
-    where p1 :: Proxy a
-          p1 = Proxy
-          p2 :: Proxy b
-          p2 = Proxy
+  graphFor rootP _ =
+    graphFor rootP (Proxy :: Proxy a) >> graphFor rootP (Proxy :: Proxy b)
